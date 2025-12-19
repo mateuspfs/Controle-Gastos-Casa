83085a96835c1e4e90790fa51b14e0cdd2ac7454
Mateus Sampaio <devmateuspfs@gmail.com>
2025-12-18 22:00:52 -0300

    feat: swagger para testar endpoints e documentar

--- api/ControleGastosCasa.Api/Program.cs
+++ api/ControleGastosCasa.Api/Program.cs
@@ -1,20 +1,28 @@
+using ControleGastosCasa.Application;
 using ControleGastosCasa.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ControleGastosCasa API",
+        Version = "v1"
+    });
+});
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
