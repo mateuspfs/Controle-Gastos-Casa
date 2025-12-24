0d2171371b53da028c3973e21744bcf1b3d9c244
Mateus Sampaio <devmateuspfs@gmail.com>
2025-12-23 22:10:11 -0300

    fix: aplicar migrações ao iniciar api

--- api/ControleGastosCasa.Api/Program.cs
+++ api/ControleGastosCasa.Api/Program.cs
@@ -2,6 +2,7 @@ using ControleGastosCasa.Application;
 using ControleGastosCasa.Infrastructure;
 using ControleGastosCasa.Infrastructure.Persistence;
 using ControleGastosCasa.Infrastructure.Seeders;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,6 +36,13 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 var app = builder.Build();
+
+// Aplica migrações automaticamente na inicialização
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+}
 
 // Executa seeders automaticamente se as tabelas estiverem vazias
 using (var scope = app.Services.CreateScope())
